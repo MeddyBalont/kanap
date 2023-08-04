b501a25680107ee3920b005523272820cd2a93c0
Meddy <105659316+MeddyBalont@users.noreply.github.com>
2023-08-04 13:55:46 +0200

    Add files via upload

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,6 +1,34 @@
+let kanap = JSON.parse(localStorage.getItem("kanap"));
 
-    // BOUCLEZ POUR ALLEZ CHERCHER LES LES ELEMENTS DU LOCALSTORAGE
-    
-    //AFFICHE LA KANAP GET_LOCALSTORAGE
+let products = [];
 
-    
+let orderId ="";
+
+function getData(productId) {
+  response = fetch('http://localhost:3000/api/products/' + productId)
+  .then(data => {
+    return data.json();
+  })
+  .catch(error =>{
+    error = 'erreur ';
+    alert(error);
+})
+console.log(response);
+//return response;
+
+}
+
+//AFFICHAGE DES PRODUITS
+
+async function affichageKanap(){
+  if (kanap === null || kanap.length ===0 ) {
+    document.querySelector("h1").textContent = "Votre panier est vide";
+  }else for (let i = 0; i < kanap.length; i++){
+    let item = kanap[i];
+    //console.log(kanap);
+
+    productData =  getData(item.id);
+    //console.log(productData);
+  }
+}
+
