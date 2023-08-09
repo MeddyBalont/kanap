c1562422df4c4e8899ace98f42424fd179928be7
Meddy <105659316+MeddyBalont@users.noreply.github.com>
2023-08-09 18:20:54 +0200

    Add files via upload

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,39 +2,41 @@ let kanap = JSON.parse(localStorage.getItem("kanap"));
 
 let products = [];
 
-let orderId ="";
-affichageKanap(kanap)
+let orderId = "";
+affichageKanap()
 
-function getData(productId) {
-fetch(`http://localhost:3000/api/products/${productId}`)
-  .then ((response) => {
-    console.log(response);
-    return response.json();
-  })
-  .then(data => {
-    //console.log(data.json());
-    //return data.json();
-  })
-  .catch(error =>{
-    error = 'erreur ';
-    alert(error);
-})
-
-//return response;
-
+async function getData(productId) {
+    return fetch(`http://localhost:3000/api/products/${productId}`)
+        .then((response) => {
+            return response.json();
+            })
+        
+        .catch(error => {
+            error = 'erreur ';
+            alert(error);
+        })
 }
 
 //AFFICHAGE DES PRODUITS
 
-async function affichageKanap(){
-  if (kanap === null || kanap.length ===0 ) {
-    document.querySelector("h1").textContent = "Votre panier est vide";
-  }else for (let i = 0; i < kanap.length; i++){
-    let item = kanap[i];
-    console.log(kanap);
+async function affichageKanap() {
+    if (kanap === null || kanap.length === 0) {
+        document.querySelector("h1").textContent = "Votre panier est vide";
+    } else for (let i = 0; i < kanap.length; i++) {
+        let item = kanap[i];
+        //console.log(kanap);
 
-    productData = getData(item.id);
-    console.log(productData);
-  }
+        const productData = await getData(item.id);
+        //console.log(productData.name);
+
+        let kanapImgContainer = document.getElementById("cart__items");
+        Objet1 = document.createElement("div");
+        Objet1.setAttribute("class", "cart__item__img")
+        let KanapImg = document.createElement("img");
+        KanapImg.setAttribute("src", kanap.image);
+        kanapImgContainer.appendChild(KanapImg);
+        
+
+
+    }
 }
-
