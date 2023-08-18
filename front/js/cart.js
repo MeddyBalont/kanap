7b49cf1de1842ae02150bccfadb8d928380f6d70
Meddy <105659316+MeddyBalont@users.noreply.github.com>
2023-08-18 15:44:13 +0200

    Add files via upload

--- front/js/cart.js
+++ front/js/cart.js
@@ -20,6 +20,8 @@ async function getData(productId) {
 //AFFICHAGE DES PRODUITS
 
 async function affichageKanap() {
+    total = 0
+    NbrArticles = 0
     if (kanap === null || kanap.length === 0) {
         document.querySelector("h1").textContent = "Votre panier est vide";
     } else for (let i = 0; i < kanap.length; i++) {
@@ -29,14 +31,65 @@ async function affichageKanap() {
         const productData = await getData(item.id);
         //console.log(productData.name);
 
-        let kanapImgContainer = document.getElementById("cart__items");
-        Objet1 = document.createElement("div");
-        Objet1.setAttribute("class", "cart__item__img")
-        let KanapImg = document.createElement("img");
-        KanapImg.setAttribute("src", kanap.image);
-        kanapImgContainer.appendChild(KanapImg);
+        let kanapItems = document.getElementById("cart__items");
+
+        //ajout de l'article:
+        let kanapArcticles = document.createElement('articles');
+        kanapItems.appendChild(kanapArcticles);
+        kanapArcticles.data_id = kanap[i]._id;
+        kanapArcticles.data_color = kanap[i]._color;
+        kanapArcticles.className = "cart__item";
+
+        //Ajout des éléments dans la div:
+        let kanapImgContainer = document.createElement("div")
+        kanapImgContainer.className = "cart__item__img";
+        kanapArcticles.appendChild(kanapImgContainer);
+
+        //Ajout de l'image:
+        let kanapImg = document.createElement("img");
+        kanapImg.src = productData.imageUrl;
+        kanapImg.aslt = productData.alt;
+        kanapImgContainer.appendChild(kanapImg); 
+
+
+        //Ajout de la div en lien "article"
+
+        let divCartItems = document.createElement("div");
+        divCartItems.className = "cart__item__content";
+        kanapArcticles.appendChild(divCartItems);
+
+        //Ajout de la div lien avec le nom, couleur et prix du produit:
+        let kanapDescription = document.createElement("div");
+        kanapDescription.className = "cart__item__description";
+        kanapArcticles.appendChild(kanapDescription);
+
+        //Ajout du "h2" nom du produit:
+        let kanapName = document.createElement("h2");
+        kanapDescription.appendChild(kanapName);
+        kanapName.innerText = productData.name;
+
+        //Ajout d'un "p" pour la coleur du produit:
+        let kanapColor = document.createElement("p")
+        kanapDescription.appendChild(kanapColor);
+        kanapColor.innerText = kanap[i].color;
+
+        //Ajout d'un "p" pour le prix du produit:
+        let KanapPrice = document.createElement("p");
+        kanapArcticles.appendChild(KanapPrice);
+        KanapPrice.innerText = `${productData.price} €`;
+
+
+
+
+
+
+        total = kanap[i].quantity*productData.price + total;
         
+        console.log((kanap[i].quantity*productData.price))
 
-
+        NbrArticles = kanap[i].quantity + NbrArticles
+        
     }
+    console.warn(total)
+    console.log(NbrArticles);
 }
